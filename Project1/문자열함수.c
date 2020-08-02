285c6b364b921297be3442c6150400fe20bffd3e
WACA <WACA@DESKTOP-V6P>
2020-08-02 13:53:18 +0900

    선택정렬

--- Project1/문자열함수.c
+++ Project1/문자열함수.c
@@ -3,37 +3,36 @@
 
 int main() {
 
-    int arr[] = { 10, 9, 11, 20, 1, 5, 3, 6, 7, 4, 2,8,13,12 };
-    int tmp = 0;
+    int arr[6] = { 3, 7, 2, 5, 1, 4 };
     int length = sizeof(arr) / sizeof(int);
+    int max, tmp;
 
-    printf("정렬 전 : \n");
-
+    printf("정렬 전 : ");
     for (int i = 0; i < length; i++) {
-        printf("%d -> ", arr[i]);
+        printf("%d ", arr[i]);
     }
 
-    printf("\n");
-
-    //-- 
-
     for (int i = 0; i < length - 1; i++) {
-        for (int j = 0; j < length - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                tmp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = tmp;
-            }
+        max = i;
+
+        // 1, 최소값을 탐색한다.
+        for (int j = i + 1; j < length; j++) {
+            if (arr[j] > arr[max])
+                max = j;
+        }
+
+        // 최소값이 자기 자신이 아닐때, 교환
+        if (i != max) {
+            tmp = arr[i];
+            arr[i] = arr[max];
+            arr[max] = tmp;
         }
     }
 
-    printf("정렬 후: \n");
 
+    printf("\n정렬 후 : ");
     for (int i = 0; i < length; i++) {
-        printf("%d -> ", arr[i]);
+        printf("%d ", arr[i]);
     }
-
-
     return 0;
-
 }
